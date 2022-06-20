e806cb0edae7f1f235a3bade68ff8c003e060594
Gabriel Boudreaux <boudreauxgabriel1@gmail.com>
2022-06-20 13:30:43 -0500

    commit tree 0b8a08f56f6a2474487e817dd84a516fd427f358

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const cardNames = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(Cardnames, event) {
+    let messages = []
+    for (let i = 0; i < Cardnames.length; i++) {
+      messages.push(`Thank you, ${Cardnames[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+  }
+
+ function countDown(positiveNumber) {
+    while (positiveNumber >= 0) { 
+        console.log(positiveNumber);
+        positiveNumber--;
+        
+    }
+ }
